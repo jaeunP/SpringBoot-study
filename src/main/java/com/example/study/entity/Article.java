b5b7ce9e56ab97da8633e3f0fdf8c9a1c713ff53
jaeunP <okn2432@gmail.com>
2022-11-16 12:56:34 +0900

    데이터를 JPA를 통하여 DB에 저장

--- src/main/java/com/example/study/entity/Article.java
+++ src/main/java/com/example/study/entity/Article.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 
 @Entity
 public class Article {
-
+    public Article() {/*Spring Data JPA 에서 Entity에 기본 생성자가 필요한 이유도 동적으로 객체 생성 시 Reflection API를 활용하기 때문*/}
     @Id //구분하기 위한 대표 값
     @GeneratedValue //자동 생성 어노테이션
     private Long id;
@@ -23,6 +23,8 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+
 
     @Override
     public String toString() {
